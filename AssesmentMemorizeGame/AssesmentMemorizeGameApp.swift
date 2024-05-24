d8cd24565c755f4035012ba6bcee38e02997bc49
Raitis ZE <raitis.zgirskis@gmail.com>
2024-05-24 23:17:29 +0300

    Initial Commit

---
+++ AssesmentMemorizeGame/AssesmentMemorizeGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssesmentMemorizeGameApp.swift
+//  AssesmentMemorizeGame
+//
+//  Created by Raitis ZE on 24/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct AssesmentMemorizeGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
